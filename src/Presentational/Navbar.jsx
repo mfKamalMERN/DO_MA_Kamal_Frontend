f6c69e027f980116cb0d514d494ba58c46a8b99d
mufaisa <mfkamalimdia@gmail.com>
2024-11-14 03:55:33 +0530

    Navbar code enhanced.

--- src/Presentational/Navbar.jsx
+++ src/Presentational/Navbar.jsx
@@ -8,12 +8,12 @@ const Navbar = (props) => {
         <nav>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMVxEQ8iS7m8rEPuuU3rqgNtiHD6YLUXtfWw&s" alt="LIC" className="logo" />
 
-
             <ul className={`navbar ${isOpen ? 'open' : ''}`}>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li className='contact'><a href="#contact">M.A.Kamal <br /> 📞: +91 9835003820</a></li>
+                {NavTabs.map((tab, index) => (
+                    <li key={index} className={tab.name === "Contact" ? 'contact' : ''}>
+                        {tab.name !== "Contact" ? <a>{tab.name}</a> : <a>{tab.value.DO} <br />{tab.value.Phone}</a>}
+                    </li>
+                ))}
             </ul>
             <button className="toggle-button" onClick={toggleNavbar}>
                 {isOpen ? 'Close' : 'Menu'} {/* Button text changes based on state */}
